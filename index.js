45f8a6d7911063c547f7112af4d179de940fe497
Web Kawsar <web.kawsarahmed@gmail.com>
2020-12-05 21:23:57 +0600

    first series done

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 
 
-const greeting = "Hi";
-const intro = (name) => {
-    console.log(greeting + " " + name);
-}
+// const greeting = "Hi";
+// const intro = (name) => {
+//     console.log(greeting + " " + name);
+// }
 
-module.exports = {
-    intro,
-    greeting
-} 
-
-
+// module.exports = {
+//     intro,
+//     greeting
+// } 
 
